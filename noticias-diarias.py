72a8701ef2f867ccff8a13131927ec98a0c9c003
Tiago Saboga <tiagosaboga@gmail.com>
2010-05-27 00:34:27 -0300

    Several improvements.

--- noticias-diarias.py
+++ noticias-diarias.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# -*- coding=utf8 -*-
+# -*- coding=utf-8 -*-
 
 # Copyright (C) 2010 Tiago Saboga <tiagosaboga@gmail.com>
 # This program is free software; you can redistribute it and/or modify
@@ -18,7 +18,7 @@
 
 import urllib2
 import re
-from BeautifulSoup import BeautifulSoup
+import BeautifulSoup
 from Tkinter import *
 
 url_nacionais = "http://www.itamaraty.gov.br/sala-de-imprensa/selecao-diaria-de-noticias/midias-nacionais"
@@ -58,14 +58,17 @@ class HyperlinkManager:
             if tag[:6] == "hyper-":
                 self.links[tag]()
                 return
-            
+
+
 class App:
     def __init__(self, master, noticias):
 
+        self.master = master
         self.frame = Frame(master)
         self.frame.pack()
         self.noticias = noticias
         self.indice = noticias.index
+        self.current = ''
 
         self.button = Button(self.frame,
                              text="QUIT",
@@ -79,14 +82,34 @@ class App:
         self.indexbutton.pack(side=LEFT)
         self.text = ()
 
-        self.insert_index()
-        
+        self.errorbutton = Button(self.frame,
+                                  text="Deu erro?",
+                                  command=self.handle_error)
+        self.errorbutton.pack(side=LEFT)
 
+        self.insert_index()
+
+    def handle_error(self):
+        import tkFileDialog, tkMessageBox, os
+        # newFrame = Toplevel()
+        # newFrame.title("Notícias diárias - Relatório de erro")
+        # newText = Text(newFrame)
+        # newText.pack()
+        if tkMessageBox.askokcancel(title="Gravação de registro de erro.",
+            message="""Você deseja gravar o registro de erro em um arquivo?
+Caso o deseje, aperte em ok e escolha o nome e a localização do arquivo.
+Envie-o a seguir para tiagosaboga@gmail.com"""):
+            f = tkFileDialog.asksaveasfile(defaultextension='txt', initialfile="erro-noticias-diarias.txt")
+            f.write(self.noticias.get_emergency_string(self.current))
+            f.close()
+                                          
     def insert_index(self):
         if not self.text:
             self.text = Text(self.frame, bg='white')
         else:
             self.remove_index()
+        if self.current:
+            self.current = ''
         self.hyperlink = HyperlinkManager(self.text)
         for name_jornal, (link_jornal, titulos) in self.indice.items():
             self.text.insert(INSERT, "%s\n" % name_jornal)
@@ -100,15 +123,13 @@ class App:
                          link_jornal,
                          link_materia)))
         self.text.pack()
-
     def insert_text(self, manchete, name_jornal, link):
+        self.current = link
         self.remove_index()
         self.text.insert(INSERT,
                          "%s" % self.noticias.get_noticia(link))
-
     def remove_index(self):
         self.text.delete('1.0', END)
-
     def get_callback(self, manchete, name_jornal, link_jornal, link_materia):
         def open_materia():
             self.insert_text(manchete, name_jornal, "%s/%s" % (link_jornal, link_materia))
@@ -131,7 +152,9 @@ vir o indicador de estado da notícia."""
         txt = get_url(link_indice)
         self.index = self.index_build(txt)
         self.noticias = {}
-
+    def get_emergency_string(self, link):
+        return "RAW\n%s\n-------------\nPRETTY\n%s" % (self.noticias[link].rawtag.prettify(),
+                                                       self.noticias[link].rawtxt)
     def get_titulos(self, jornal):
         """jornal is a BeautifulTag"""
         titulos = []
@@ -141,10 +164,9 @@ vir o indicador de estado da notícia."""
             link = m.a['href']
             titulos.append((name, link))
         return titulos
-
     def index_build(self, txt):
         index = {}
-        soup = BeautifulSoup(txt)
+        soup = BeautifulSoup.BeautifulSoup(txt)
         tabl = soup.first('ul', {'yal:omit-tag': ''})
         jornais = tabl.findAll(name="ul", recursive=False)
         for j in jornais:
@@ -153,72 +175,115 @@ vir o indicador de estado da notícia."""
             titulos = self.get_titulos(j)
             index[name]=(link, titulos)
         return index
-
     def get_noticia(self, link):
         noticia = self.noticias.get(link, None)
         if not noticia:
-            txt = get_url(link)
-            noticia = self.extract_noticia(txt)
+            noticia = Noticia(link)
             self.noticias[link] = noticia
-        return noticia
+        return noticia.gettext()
 
-    def extract_noticia_orig(self, txt):
-        soup = BeautifulSoup(txt)
-        noticia = soup.find('div', {'id': 'content'})
-        return noticia
 
+
+class Noticia(object):
+    def __init__(self, link):
+        self.link = link
+        self.rawtxt = get_url(link)
+        self.rawtag = self.extract_noticia(self.rawtxt)
+        try:
+            self.title = self.rawtag.h1
+            self.date = self.rawtag.find('p', text=re.compile('^\d{2}/\d{2}/\d{4}$'))
+            self.name = self.date.findNext('p')
+            self.section = self.name.findNext('span')
+            self.subtitle = self.rawtag.find('div', {'id': 'subTituloAreaMateria'})
+##            self.text = (self.rawtag.find('div', {'id': 'textoAreaMateria'})
+##                    or
+##                    self.rawtag.find('div', {'class': "documentDescription"}))
+##            title, date, name, section, subtitle = self.tostring(title, date, name, section, subtitle)
+            self.text = self.extract_text_noticia(self.rawtag)   #(text)
+            if not self.text:
+                print noticia.prettify()
+                raise Exception
+        except AttributeError:
+            print noticia.prettify()
+            raise
+    def gettext(self):
+        #print self.rawtag
+        #print "$$$%s$$$" % self.text
+        ret = ''
+        for data in [self.title,
+                     self.date,
+                     self.name,
+                     self.section,
+                     self.subtitle,
+                     self.text]:
+            try:
+                ret += "\n%s" % data
+            except:
+                continue
+        return ret
+    def extract_noticia(self, txt):
+        soup = BeautifulSoup.BeautifulSoup(txt)
+        try:
+            noticia = soup.find('div', {'id': 'content'})
+        except:
+            return txt
+        return noticia
+    def _descend_into_tags(self, tag):
+        txt = []
+        for item in tag:
+            if type(item) is BeautifulSoup.NavigableString:
+                txt.append(item.replace('\r\n', ' '))
+            elif type(item) is BeautifulSoup.Tag:
+                if len(item)>0:
+                    self._descend_into_tags(item)
+                else:
+                    if item.name=='br':
+                        if txt and txt[-1]!='\n':
+                            txt.append('\n')
+                    else:
+                        txt.append(item.name)
+        return txt
+    def extract_text_noticia(self, tag):
+        txt = []
+        for p in tag.findAll('p'):
+            print "entering p"
+            txt.extend(self._descend_into_tags(p))
+        return '\n'.join(txt)   
     def tostring(self, *args):
         ret = []
         for arg in args:
+            # print type(arg)
             try:
                 st = arg.string
             except AttributeError:
                 st = unicode(arg)
+            if not st:
+                st = ''
+                for a in arg:
+                    try:
+                        st += self.tostring(a)
+                    except:
+                        # print "st: %s\ntype: %s" % (st, type(st))
+                        pass
             ret.append(st)
         return ret
-
-    def extract_noticia(self, txt):
-        soup = BeautifulSoup(txt)
-        try:
-            noticia = soup.find('div', {'id': 'content'})
-            print noticia.prettify()
-            title = noticia.h1
-            date = noticia.find('p', text=re.compile('^\d{2}/\d{2}/\d{4}$'))
-            name = date.findNext('p')
-            section = name.findNext('span')
-            subtitle = noticia.find('div', {'id': 'subTituloAreaMateria'})
-            text = noticia.find('div', {'id': 'textoAreaMateria'})
-            print text.prettify()
-            title, date, name, section, subtitle, text = self.tostring(title, date, name, section, subtitle, text)
-            print '1---------------1'
-            print text
-        except:
-            raise
-        return '\n'.join([title, date, name, section, subtitle, text])
 
 def get_url(url):
     headers = {'Accept': 'text/html'}
     rqs = urllib2.Request(url=url, headers=headers)
     return urllib2.urlopen(rqs).read()
 
+def save_file(filename, txt):
+    import os.path
+    f=open(filename, 'w')
+    f.write(txt)
+    f.close()
+
 def main():
     noticias = Noticias(url_nacionais)
-    # txt_indice_diario = get_url(url_nacionais)
-    # indice_noticias = index_build(txt_indice_diario)
-
     root = Tk()
     app = App(root, noticias)
-    # w = Label(root, text="Baixando notícias")
-    # w.pack()
-
     root.mainloop()
-
-
-    # for jornal, indice in indice_noticias.items():
-    #     print "\n\n%s: \n" % jornal
-    #     for noticia in indice[1]: # noticia is a tuple (name, link)
-    #         print noticia[0] # only name
-    
 
 if __name__=="__main__":
     main()
